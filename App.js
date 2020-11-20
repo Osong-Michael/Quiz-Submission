f1211a3f9917d12d0755d148ed9a6c942add61d3
Osong Agberndifor <38656549+OA7@users.noreply.github.com>
2020-11-20 10:06:11 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,30 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import _ from 'lodash';
 import data from './data';
 import BarChart from './BarChart';
 
 export default function App() {
-  const [state, setState] = useState(data);
+  const newData = _.merge(data.spending, data.income);
+  const spent = newData[0].spending;
+  const incoming = newData[0].income;
+  const newState = {
+    spent,
+    incoming,
+  };
+
+  const [state, setState] = useState(newState);
+  const bHeigth = data.overall_budget / 10;
+  
+
+  const _onPressButton = (data) => {
+    setState({
+      spent: data.spending,
+      incoming: data.income,
+    });
+  }
+
+  const styleLine = {
+    borderTopWidth: 1,
+    borderTopColor: '#fff',
+    top: bHeigth,
+    flex: 0.5,
+    width: '94%',
+    marginHorizontal: 10
+  }
+
+  const styleCircle1 = {
+    borderWidth: 1,
+    borderRadius: 10,
+    marginRight: 5,
+    height: 15,
+    width: 15,
+    backgroundColor: '#428DFC',
+  }
+
+  const styleCircle2 = {
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 15,
+    width: 15,
+    backgroundColor: '#0CE381',
+  }
+  
   
   return (
     <View style={styles.container}>
       <Text style={styles.textBoro}>October</Text>
       <View style={{width: 250, flexDirection: 'row',}}>
-        <Text style={styles.spending}>Spending</Text>
-        <Text style={styles.income}>Income</Text>
+        <Text style={styles.spending}><View style={StyleSheet.flatten(styleCircle1)} />Spending</Text>
+        <Text style={styles.income}><View style={StyleSheet.flatten(styleCircle2)} />Income</Text>
       </View>
       <View style={{width: 250, flexDirection: 'row', marginTop: 5}}>
-        <Text style={{color: '#fff', paddingLeft: 25}}>$200</Text>
-        <Text style={{color: '#fff', paddingLeft: 50}}>$500</Text>
+  <Text style={{color: '#fff', paddingLeft: 40}}>${state.spent}</Text>
+  <Text style={{color: '#fff', paddingLeft: 65}}>${state.incoming}</Text>
       </View>
-      <View style={{width: '100%', height: 300}}>
-        <View style={{left: '80%', top: '45%'}}>
+      <View style={{width: '100%', height: 200}}>
+        <View style={{left: '80%', top: '30%'}}>
           <Text style={{color: '#fff'}}>Budget</Text>
-          <Text style={{color: '#fff'}}>${state.overall_budget}</Text>
+          <Text style={{color: '#fff'}}>${data.overall_budget}</Text>
         </View>
-        <Text style={{borderTopWidth: 1, borderTopColor: '#fff', top: 150, flex: 0.5, width: '95%', marginHorizontal: 10}} />
-        <BarChart spending={state.spending} income={state.income} />
+        <View style={{height: 200}}>
+          <Text style={StyleSheet.flatten(styleLine)} />
+          <BarChart newData={newData} _onPressButton={_onPressButton} />
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -38,7 +85,8 @@ const styles = StyleSheet.create({
   },
   textBoro: {
     color: '#fff',
-    marginTop: 60,
+    marginTop: 120,
+    marginBottom: 10,
     fontSize: 25,
     paddingLeft: 25,
   },
